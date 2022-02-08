f2d4c3dfd5c022e1a58467a016c4319fcf9d52f1
Ren Wang <iamwrm@gmail.com>
2022-02-08 21:22:53 +0800

    do clean up

--- scripts/build_for_linux.sh
+++ scripts/build_for_linux.sh
@@ -9,4 +9,5 @@ make -C linux-${VERSION}/tools/perf
 mkdir linux_${VERSION}_perf
 mv linux-${VERSION}/tools/perf linux_${VERSION}_perf/
 tar cvzf linux_${VERSION}_perf.tar.gz ./linux_${VERSION}_perf
+rm -rf v${VERSION}.zip linux-${VERSION}
 
