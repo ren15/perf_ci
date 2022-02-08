ac80edfc53f483824bd0cce5fe90c7f6fef20674
Ren Wang <iamwrm@gmail.com>
2022-02-08 21:21:50 +0800

    use scripts

---
+++ scripts/build_for_linux.sh
@@ -0,0 +1,12 @@
+set -xe
+
+# for example 5.4
+VERSION=$1
+
+wget -c -q https://github.com/torvalds/linux/archive/refs/tags/v${VERSION}.zip
+unzip -q v${VERSION}.zip
+make -C linux-${VERSION}/tools/perf
+mkdir linux_${VERSION}_perf
+mv linux-${VERSION}/tools/perf linux_${VERSION}_perf/
+tar cvzf linux_${VERSION}_perf.tar.gz ./linux_${VERSION}_perf
+
